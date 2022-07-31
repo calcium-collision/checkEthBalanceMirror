2d1239edc6e7e1b28b8607675960154080859b5e
HIYJIH <kolian200224@gmail.com>
2022-07-31 20:29:45 +0300

    etherscan api

--- main.py
+++ main.py
@@ -1,53 +1,30 @@
+import os
+
 from flask import Flask
 import requests
-from bs4 import BeautifulSoup
 app = Flask(__name__)
-from requests_html import HTMLSession
 
-
-cookie = {'ASP.NET_SessionId': 'rmywxvite4frbe1pc0umvg1j',
-            '__cflb': '02DiuFnsSsHWYH8WqVXbZzkeTrZ6gtmGV6uegZWsYZSJx'}
-header = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36" ,
-    "Accept-Language":"ru,en-GB;q=0.9,en-US;q=0.8,en;q=0.7"
-}
-
-
-@app.route("/")
-def checkVersion():
-    session = HTMLSession()
-    res = session.get("https://etherscan.io/", headers=header)
-    return dict(res.cookies)
-
-
+END_POINT = "https://api.etherscan.io/api"
 
 @app.route("/<string:wallet>")
 def balance(wallet):
-    # # Check password
-    # password = request.args.get('pass', None)
-    # if password == None or password != "goodpassSheesh":
-    #     return "Wrong password"
-
-    # session = requests.Session()
-    # res = session.get("https://etherscan.io/", headers = header)
-    # cookies = dict(res.cookies)
-    # res = session.get("https://etherscan.io/address/" + wallet, headers = header, cookies=cookies)
-
-    session = HTMLSession()
-    res = session.get("https://etherscan.io/address/0x9791CeFCAb198607037DC8ee8d70358142C9EF6d" + wallet, headers = header, cookies=cookie)
-
-    return getBalance(res)
-
-def getBalance(site):
     try:
-        soup = BeautifulSoup(site.text, "html.parser")
-        balanceSoup = soup.findAll(class_='col-md-8')
-        lineBalance = balanceSoup[0].getText()
-        balance = lineBalance.split()[0][0:4]
+        params = {
+            "module": "account",
+            "action": "balance",
+            "address": wallet,
+            "tag": "latest",
+            "apikey": os.environ.get("API_KEY")
+        }
 
-        return balance
+        res = requests.get(END_POINT, params=params).json()
+        balanceWeiValue = res['result']
+        print(balanceWeiValue)
+        balanceEthValue = int(balanceWeiValue) / 1000000000000000000
+        return str(round(balanceEthValue, 2))
     except:
-        return "Somthing wrong (￣(工)￣) 5\n" + BeautifulSoup(site.text, "html.parser").text
+        return "Something wrong (￣(工)￣) \n"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
